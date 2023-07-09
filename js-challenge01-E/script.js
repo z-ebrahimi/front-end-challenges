a0eb87993bfaa53635ad5acbd42151436e04ef22
Zahra-90 <134717099+Zahra-90@users.noreply.github.com>
2023-07-10 02:06:38 +0430

    js-challenge01

---
+++ js-challenge01-E/script.js
@@ -0,0 +1,6 @@
+const app = document.getElementById("app");
+app.innerHTML = `
+    <div class="welcome-text" style="color: red;text-weight: bold;height:300px;line-height:300px;text-align:center;">
+        <h2> Welcome To JavaScript </h2>
+    </div>
+    `;
